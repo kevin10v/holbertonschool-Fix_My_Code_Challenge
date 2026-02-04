3e1aea090cd808f5b8785b9074e822188ef1a57d
kevin10v <vokakevin@icloud.com>
2026-02-04 13:50:43 +0100

    Fix task 0: FizzBuzz without trailing space

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -12,16 +12,17 @@ def fizzbuzz(n):
     - For multiples of five print "Buzz" instead of the number
     - For multiples of both three and five print "FizzBuzz"
     """
+    result = []
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
-            print("FizzBuzz", end=' ')
+            result.append("FizzBuzz")
         elif (i % 3) == 0:
-            print("Fizz", end=' ')
+            result.append("Fizz")
         elif (i % 5) == 0:
-            print("Buzz", end=' ')
+            result.append("Buzz")
         else:
-            print(i, end=' ')
-    print("")
+            result.append(str(i))
+    print(" ".join(result))
 
 
 if __name__ == '__main__':
